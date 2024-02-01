866dda44f7e0685bcdd64e6cf2bf64a86586e636
Tony <qkrdkwl9090@naver.com>
2024-02-01 12:59:59 +0900

    feat: set global style, theme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const firstTheme: DefaultTheme = {
+  primaryColor: "#178da1",
+};
+
+export const secondTheme: DefaultTheme = {
+  primaryColor: "#efc235",
+};
+
+export const thirdTheme: DefaultTheme = {
+  primaryColor: "#c64a75",
+};
